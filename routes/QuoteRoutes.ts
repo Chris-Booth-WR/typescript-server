ac79098fd2a8dab0d81040e7348373903436a70b
Chris-Booth-WR <cbooth@worldremit.com>
2021-01-07 14:59:45 +0000

    Added auth to quote controller

--- routes/QuoteRoutes.ts
+++ routes/QuoteRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { authenticateJWT } from "../middleware/AuthenticaionMiddleware"
 import { IMarketingRequest } from "../models/IMarketingRequest";
 import { RepoBase } from "../repository/IRepository";
 import { LocalMarketing } from "../repository/marketing/LocalMarketing";
@@ -13,9 +14,9 @@ export class QuoteRoutes extends CommonRoutesConfig {
 
     configureRoutes() {
         this.app.route(`/quote`)
-            .get((req: express.Request, res: express.Response) => {
+            .get([authenticateJWT, (req: express.Request, res: express.Response) => {
                 res.status(200).json(this.repo.list(""));
-            })
+            }])
         return this.app;
     }
 }
